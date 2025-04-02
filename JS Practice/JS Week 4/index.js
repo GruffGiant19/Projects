8d05391ee7916e28450336e7b76c2235a544bef4
Derrick Arhin-Bannerman <derrickarhinbannerman@gmail.com>
2025-04-02 11:21:12 +0000

    New

---
+++ JS Practice/JS Week 4/index.js
@@ -0,0 +1,20 @@
+  //Creating an Object 
+  const Twin = {};
+  Twin.name = "Eric Arhin-Bannerman";
+  Twin.email = "ericarhinbannerman@gmail.com";
+  Twin.age = 30;
+
+  let Me = {
+      name : "Derrick Arhin-Bannerman",
+      email : "derrickarhinbannerman@gmail.com",
+      age : 30, 
+  }
+
+  let Area = {
+      length : 27,
+      width : 15,
+  }
+
+  console.log(Me);
+  console.log(Twin);
+  console.log(Area);
