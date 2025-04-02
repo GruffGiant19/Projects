05fed2e35be41703e1fa8c17a52c101429858040
Derrick Arhin-Bannerman <derrickarhinbannerman@gmail.com>
2025-04-02 11:21:31 +0000

    This is a new commit

--- JS Practice/JS Week 4/index.js
+++ JS Practice/JS Week 4/index.js
@@ -1,20 +1,46 @@
-  //Creating an Object 
-  const Twin = {};
-  Twin.name = "Eric Arhin-Bannerman";
-  Twin.email = "ericarhinbannerman@gmail.com";
-  Twin.age = 30;
+//   //Creating an Object 
+//   const Twin = {};
+//   Twin.name = "Eric Arhin-Bannerman";
+//   Twin.email = "ericarhinbannerman@gmail.com";
+//   Twin.age = 30;
 
-  let Me = {
-      name : "Derrick Arhin-Bannerman",
-      email : "derrickarhinbannerman@gmail.com",
-      age : 30, 
+//   let Me = {
+//       name : "Derrick Arhin-Bannerman",
+//       email : "derrickarhinbannerman@gmail.com",
+//       age : 30, 
+//   }
+
+//   let Area = {
+//       length : 27,
+//       width : 15,
+//   }
+
+//   console.log(Me);
+//   console.log(Twin);
+//   console.log(Area);
+
+
+  class Human {
+    constructor(name,age) {
+        this.name = name;
+        this.age = age;
+  
+    }
+
+    talk() {
+        console.log(` ${this.name} speaks`);
+    }
   }
 
-  let Area = {
-      length : 27,
-      width : 15,
-  }
+  class Man extends Human {
+    constructor(name,age) {
+        super(name, age);
+    }
+}
+    const man = new Man ("Kofi",27);
+    man.talk();
 
-  console.log(Me);
-  console.log(Twin);
-  console.log(Area);
+  
+
+  
+  //Object Inheritance
