3d075829ccd5d99e8a7ccb11105d995e4c489fdc
Derrick Arhin-Bannerman <derrickarhinbannerman@gmail.com>
2025-05-22 13:41:58 +0000

    New changes

--- React Practice/first-project/src/App.jsx
+++ React Practice/first-project/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Derrick = () => {
+
+  const name = "Derrick";
+  const age = "32";
+
+  const increase= () => {
+    const num = document.getElementsByTagName("button").innerHTML;
+    num++;
+
+    return num++;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>My name is {name}. I am {age} years old. </h2>
+      <h2>I am male.</h2>
+      <button onClick={increase}>{age}</button>
+    </div>
   )
 }
 
-export default App
+export default Derrick
